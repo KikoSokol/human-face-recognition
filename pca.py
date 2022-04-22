7b9fa79e66131012560aaa94eeadbab1f8f4b289
Kristián Sokol <kikosokol@gmail.com>
2022-04-22 08:59:05 +0200

    PCA - počítanie pca príznakov, vykresľovanie roc krivky pre 10 obrázkov z každého videa každý z každým, a pre average obrázky

--- pca.py
+++ pca.py
@@ -79,27 +79,103 @@ def get_data_for_pca(directory):
     return (pair_one_img, pair_one_labels, pair_one_dict_names), (pair_two_img, pair_two_labels, pair_two_dict_names)
 
 
-def pca(data):
+def get_all_data_for_pca(directories):
+    images = []
+    labels = []
+    dict_names = {}
+
+    label = 0
+
+    for main_dir in directories:
+        for pair_directory in get_directory(main_dir):
+            pairs_directory = []
+            for image_directory in get_directory(pair_directory):
+                pairs_directory.append(image_directory)
+
+            pair1 = pairs_directory[0]
+            pair2 = pairs_directory[1]
+
+            get_pca_from_image_directory(pair1, images, labels, label)
+            dict_names[label] = pair1
+            label += 1
+
+            get_pca_from_image_directory(pair2, images, labels, label)
+            dict_names[label] = pair2
+            label += 1
+
+    return images, labels, dict_names
+
+
+# def pca(data):
+#     images = data[0]
+#     labels = data[1]
+#     dict_names = data[2]
+#     pca = PCA().fit(images)
+#     print(pca.explained_variance_ratio_)
+#
+#     n_components = 50
+#     eigenfaces = pca.components_[:n_components]
+#
+#     weights = eigenfaces @ (images - pca.mean_).T
+#
+#     example_image = get_image("TRUE/ALL/Linda_Dano_2-Linda_Dano_3/Linda_Dano_3/Linda_Dano_3_0.jpg")
+#     query = example_image.reshape(1, -1)
+#     query_weight = eigenfaces @ (query - pca.mean_).T
+#     euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
+#     best_match = np.argmin(euclidean_distance)
+#     print("best_match: ", best_match)
+#     print("Best match %s with Euclidean distance %f" % (dict_names[labels[best_match]], euclidean_distance[best_match]))
+
+
+def save_pca_weight_for_video(directory, pca_dict, eigenfaces, mean):
+    for image in get_files(directory):
+        img = Image.open(image)
+        img = asarray(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.flatten()
+        weight = eigenfaces @ (img - mean).T
+        pca_dict[image] = weight
+
+
+def do_pca(directories, eigenfaces, mean):
+    pca_dict = {}
+
+    for main_dir in directories:
+        for pair_directory in get_directory(main_dir):
+            pairs_directory = []
+            for image_directory in get_directory(pair_directory):
+                pairs_directory.append(image_directory)
+
+            pair1 = pairs_directory[0]
+            pair2 = pairs_directory[1]
+
+            save_pca_weight_for_video(pair1, pca_dict, eigenfaces, mean)
+            save_pca_weight_for_video(pair2, pca_dict, eigenfaces, mean)
+
+    return pca_dict
+
+
+def pca(directories, data):
     images = data[0]
     labels = data[1]
     dict_names = data[2]
-    pca = PCA().fit(images)
-    print(pca.explained_variance_ratio_)
 
     n_components = 50
+    pca = PCA(n_components=n_components).fit(images)
+    # print(pca.explained_variance_ratio_)
+
     eigenfaces = pca.components_[:n_components]
 
     weights = eigenfaces @ (images - pca.mean_).T
 
-    example_image = get_image("TRUE/ALL/Linda_Dano_2-Linda_Dano_3/Linda_Dano_3/Linda_Dano_3_0.jpg")
-    query = example_image.reshape(1, -1)
-    query_weight = eigenfaces @ (query - pca.mean_).T
-    euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
-    best_match = np.argmin(euclidean_distance)
-    print("best_match: ", best_match)
-    print("Best match %s with Euclidean distance %f" % (dict_names[labels[best_match]], euclidean_distance[best_match]))
+    return do_pca(directories, eigenfaces, pca.mean_), weights
 
 
-one_true, two_true = get_data_for_pca(DIRECTORY_TRUE)
+# one_true, two_true = get_data_for_pca(DIRECTORY_TRUE)
 
-pca(one_true)
+
+def get_pca_weights():
+    directories = [DIRECTORY_TRUE, DIRECTORY_FALSE]
+    data = get_all_data_for_pca(directories)
+
+    return pca(directories, data)
---
+++ pca_weights_comparator.py
@@ -0,0 +1,212 @@
+import os
+from PIL import Image
+from matplotlib import pyplot as plt
+from numpy import asarray
+import cv2
+from scipy.spatial import distance
+import pca as p
+import random
+from sklearn.decomposition import PCA
+
+DIRECTORY_TRUE = "TRUE/ALL/"
+DIRECTORY_FALSE = "FALSE/ALL/"
+
+
+def get_directory(path):
+    directory = []
+    for filename in os.listdir(path):
+        directory.append(path + filename + "/")
+    return directory
+
+
+def get_files_without_average(path):
+    files = []
+    for filename in os.listdir(path):
+        if "AVERAGE" not in filename:
+            files.append(path + filename)
+
+    for i in files:
+        print(i)
+    return files
+
+
+def get_images_without_average(directory):
+    img_dict = {}
+
+    image_files = get_files_without_average(directory)
+
+    for i in range(0, min(10, len(image_files))):
+        image = image_files[i]
+        img = Image.open(image)
+        img = asarray(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.flatten()
+        img_dict[image] = img
+
+    return img_dict
+
+
+def compute_distances(pair1_videos, pair2_videos, image_weights, weights):
+    distances = []
+
+    for file_name_1, img1 in pair1_videos.items():
+        for file_name_2, img2 in pair2_videos.items():
+            img1_weight = image_weights[file_name_1]
+            img2_weight = image_weights[file_name_2]
+            distances.append(distance.euclidean(img1_weight, img2_weight))
+
+    return distances
+
+
+def compare_without_average(directories, image_weights, weights):
+    all_distances = []
+
+    for pair_directory in get_directory(directories):
+        pairs_directory = []
+        for image_directory in get_directory(pair_directory):
+            pairs_directory.append(image_directory)
+
+        pair1 = pairs_directory[0]
+        pair2 = pairs_directory[1]
+        pair1_videos = get_images_without_average(pair1)
+        pair2_videos = get_images_without_average(pair2)
+        all_distances.extend(compute_distances(pair1_videos, pair2_videos, image_weights, weights))
+
+    return all_distances
+
+
+# AVERAGE
+
+def get_files_average(path):
+    files = []
+    for filename in os.listdir(path):
+        if "AVERAGE" in filename:
+            files.append(path + filename)
+
+    for i in files:
+        print(i)
+    return files
+
+
+def get_images_average(directory):
+    img_dict = {}
+
+    image_files = get_files_average(directory)
+
+    for i in range(0, min(10, len(image_files))):
+        image = image_files[i]
+        img = Image.open(image)
+        img = asarray(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.flatten()
+        img_dict[image] = img
+
+    return img_dict
+
+
+def compare_average(directories, image_weights, weights):
+    all_distances = []
+
+    for pair_directory in get_directory(directories):
+        pairs_directory = []
+        for image_directory in get_directory(pair_directory):
+            pairs_directory.append(image_directory)
+
+        pair1 = pairs_directory[0]
+        pair2 = pairs_directory[1]
+        pair1_videos = get_images_average(pair1)
+        pair2_videos = get_images_average(pair2)
+        all_distances.extend(compute_distances(pair1_videos, pair2_videos, image_weights, weights))
+
+    return all_distances
+
+
+def distance_without_average():
+    image_weights, weights = p.get_pca_weights()
+
+    distances_true = compare_without_average(DIRECTORY_TRUE, image_weights, weights)
+    distances_false = compare_without_average(DIRECTORY_FALSE, image_weights, weights)
+
+    return distances_true, distances_false
+
+
+def distance_average():
+    image_weights, weights = p.get_pca_weights()
+
+    distances_true = compare_average(DIRECTORY_TRUE, image_weights, weights)
+    distances_false = compare_average(DIRECTORY_FALSE, image_weights, weights)
+
+    return distances_true, distances_false
+
+
+def compute_roc_tpr_fpr(distances_true, trashold):
+    tpr_or_fpr = 0
+
+    for dist in distances_true:
+        if dist < trashold:
+            tpr_or_fpr += 1
+
+    return tpr_or_fpr / len(distances_true)
+
+
+def compute_roc_data(distances):
+    trasholds = []
+    tprs = []
+    fprs = []
+
+    all = []
+    all.extend(distances[0])
+    all.extend(distances[1])
+
+    # min distance
+    trashold_for_min = min(all)
+    trasholds.append(trashold_for_min)
+    tprs.append(compute_roc_tpr_fpr(distances[0], trashold_for_min))
+    fprs.append(compute_roc_tpr_fpr(distances[1], trashold_for_min))
+
+    iteration = 20
+    for trashold in range(int(min(all) + iteration), int(max(all)), iteration):
+        trasholds.append(trashold)
+        tprs.append(compute_roc_tpr_fpr(distances[0], trashold))
+        fprs.append(compute_roc_tpr_fpr(distances[1], trashold))
+
+    #     max distance
+    trashold_for_max = max(all)
+    trasholds.append(trashold_for_max)
+    tprs.append(compute_roc_tpr_fpr(distances[0], trashold_for_max))
+    fprs.append(compute_roc_tpr_fpr(distances[1], trashold_for_max))
+
+    print("len", len(trasholds))
+    return trasholds, tprs, fprs
+
+
+def show_roc(roc_data_all, roc_data_average):
+    plt.figure()
+    lw = 2
+    plt.plot(
+        roc_data_all[2],
+        roc_data_all[1],
+        color="darkorange",
+        lw=lw,
+        # label="ROC curve (area = %0.2f)" % roc_auc[2],
+    )
+
+    plt.plot(
+        roc_data_average[2],
+        roc_data_average[1],
+        color="navy",
+        lw=lw,
+        # label="ROC curve (area = %0.2f)" % roc_auc[2],
+    )
+
+    plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Receiver operating characteristic example")
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+show_roc(compute_roc_data(distance_without_average()), compute_roc_data(distance_average()))
